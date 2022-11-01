18961d7fd0ca6684ed8f218c5af51674d4e180e7
Toño Ramos <t.ramos@renr.es>
2022-11-01 13:07:24 +0100

    Entidad, repositorio y migraciones

---
+++ app/migrations/Version20221101120635.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221101120635 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE planet (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, rotation_period INT DEFAULT NULL, orbital_period INT DEFAULT NULL, diameter INT NOT NULL, films_count INT DEFAULT NULL, created DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE planet');
+    }
+}
---
+++ app/src/Entity/Planet.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlanetRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlanetRepository::class)]
+class Planet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $rotation_period = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $orbital_period = null;
+
+    #[ORM\Column]
+    private ?int $diameter = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $films_count = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRotationPeriod(): ?int
+    {
+        return $this->rotation_period;
+    }
+
+    public function setRotationPeriod(?int $rotation_period): self
+    {
+        $this->rotation_period = $rotation_period;
+
+        return $this;
+    }
+
+    public function getOrbitalPeriod(): ?int
+    {
+        return $this->orbital_period;
+    }
+
+    public function setOrbitalPeriod(?int $orbital_period): self
+    {
+        $this->orbital_period = $orbital_period;
+
+        return $this;
+    }
+
+    public function getDiameter(): ?int
+    {
+        return $this->diameter;
+    }
+
+    public function setDiameter(int $diameter): self
+    {
+        $this->diameter = $diameter;
+
+        return $this;
+    }
+
+    public function getFilmsCount(): ?int
+    {
+        return $this->films_count;
+    }
+
+    public function setFilmsCount(?int $films_count): self
+    {
+        $this->films_count = $films_count;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+}
